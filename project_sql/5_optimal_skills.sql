37b555f400ec1cfe5d02a2cce3d386bf721ef10d
Bean-Dip53 <242837393+Bean-Dip53@users.noreply.github.com>
2025-11-15 16:18:15 -0500

    upload query 5 update

--- project_sql/5_optimal_skills.sql
+++ project_sql/5_optimal_skills.sql
@@ -50,3 +50,10 @@ SELECT
 FROM   
     skills_demand
 INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
+WHERE
+    demand_count > 10 
+ORDER BY
+    avg_salary DESC,
+    demand_count DESC
+LIMIT
+    25
